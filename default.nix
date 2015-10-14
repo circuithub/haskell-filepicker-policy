6d0fdb00395058f90d4b5a8cdba07da10801ad24
Oliver Charles <ollie@ocharles.org.uk>
2015-10-14 11:26:28 +0100

    nix: Add default.nix
    Automatically generated by cabal2nix

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ mkDerivation, aeson, base, base64-bytestring, bytestring
+, containers, cryptonite, hspec, HUnit, memory, raw-strings-qq
+, stdenv, text, time, utf8-string
+}:
+mkDerivation {
+  pname = "filepicker-policy";
+  version = "0.2.0";
+  src = ./.;
+  buildDepends = [
+    aeson base base64-bytestring bytestring containers cryptonite
+    memory text time utf8-string
+  ];
+  testDepends = [
+    aeson base bytestring hspec HUnit raw-strings-qq time
+  ];
+  description = "API for creating filepicker.com policies";
+  license = stdenv.lib.licenses.mit;
+}
